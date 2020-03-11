a66d5f7f9892f370a6a754ce6b0ebb83ce75b83b
Michael Singhurse <michaelsinghurse@gmail.com>
2020-03-11 22:38:25 +0000

    started on testing suite

---
+++ todolist.test.js
@@ -0,0 +1,28 @@
+// todolist.test.js
+
+const Todo = require("./todo");
+const TodoList = require("./todolist");
+
+describe("TodoList", () => {
+  let todo1;
+  let todo2;
+  let todo3;
+  let list;
+  
+  beforeEach(() => {
+    todo1 = new Todo("Buy milk");
+    todo2 = new Todo("Clean room");
+    todo3 = new Todo("Go to the gym");
+    
+    list = new TodoList("Today's Todos");
+    list.add(todo1);
+    list.add(todo2);
+    list.add(todo3);
+  });
+  
+  test('todolist has a size of 3', () => {
+    expect(list.size()).toBe(3);
+  });  
+  
+  
+});
